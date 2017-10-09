4e78334d277b89180114fc6c207c502a3d862aa6
abelvm <abel@cartodb.com>
2017-10-09 17:02:08 +0200

    vodafone custom code

--- sql/CDB_Quadgrid_recursive.sql
+++ sql/CDB_Quadgrid_recursive.sql
@@ -13,7 +13,7 @@ BEGIN
         UNION ALL
         SELECT
             t.id, t.id ||'||quote_literal('-')||'|| (row_number() over())::text,x*2 + xx,y*2 + yy,z+1,
-            (SELECT count(*) FROM '||tablename||' WHERE the_geom_webmercator && CDB_XYZ_Extent(x*2 + xx, y*2 + yy, z+1))
+            (SELECT count(*) FROM '||tablename||' WHERE ST_Intersects(the_geom_webmercator,CDB_XYZ_Extent(x*2 + xx, y*2 + yy, z+1))
         FROM t, (VALUES (0, 0), (0, 1), (1, 1), (1, 0)) as c(xx, yy)
         WHERE e >= '||threshold||' AND z < 25
     ),
@@ -81,6 +81,91 @@ END
 $$ LANGUAGE 'plpgsql' IMMUTABLE STRICT;
 
 
--- //////// test recursion on 150K ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+-- //////// test recursion on 150K ///////////////////////////////////////////////////////////////////////////////////////////
 
 SELECT * FROM CDB_QuadGrid_r2('arbrat', 25);
+
+
+-- /////// VODAFONE /////////////////////////////////////////////////////////////////////////////////////////////////////////
+-- unique customers, per dow and hour //////////////////////////////////////////////////////////////////////////////////////
+
+-- add dow and hour
+SELECT
+    clientid,
+    extract(isodow from timestamp) as dow,
+    extract(hour from timestamp) as h,
+    the_geom
+FROM sample
+
+-- raw query
+WITH
+RECURSIVE t(pid, id, x, y, z, dow, h, e) AS (
+    SELECT
+        '','0',0,0,0, dow, h, count(*) FROM sample_dowh group by dow, h
+    UNION ALL
+    SELECT
+        t.id, t.id ||'-'|| (row_number() over())::text,x*2 + xx,y*2 + yy,z+1,
+      t.dow, t.h, (SELECT count(distinct clientid) FROM sample_dowh WHERE the_geom_webmercator && CDB_XYZ_Extent(x*2 + xx, y*2 + yy, z+1) and dow =t.dow and h=t.h)
+    FROM t, (VALUES (0, 0), (0, 1), (1, 1), (1, 0)) as c(xx, yy)
+    WHERE e >= 25 AND z < 25
+),
+potential as(SELECT pid, id, x, y, z, dow, h, e FROM t WHERE e >= 25),
+cleaned as(
+    SELECT x, y, z, dow, h, e, coalesce(c, 0) as c
+    FROM
+        potential p1
+    left join
+        lateral(SELECT count(1) as c FROM potential where pid = p1.id) p2
+    ON 1=1
+)
+SELECT
+    dow, h,
+    ST_transform(CDB_XYZ_Extent(x, y, z), 3857) as the_geom,
+    e as occurrences
+FROM cleaned
+WHERE c = 0
+
+-- function
+CREATE OR REPLACE FUNCTION CDB_QuadGrid_vodafone(
+    IN tablename regclass,
+    IN threshold integer
+    )
+RETURNS TABLE(the_geom geometry, occurrences bigint)  AS $$
+BEGIN
+    RETURN QUERY EXECUTE
+   'WITH
+    RECURSIVE t(pid, id, x, y, z, dow, h, e) AS (
+        SELECT
+            '||quote_literal('')||','||quote_literal('0')||',0,0,0, dow, h, count(distinct clientid) FROM '||tablename||' group by dow, h
+        UNION ALL
+        SELECT
+            t.id,
+            t.id ||'||quote_literal('-')||'|| (row_number() over())::text,
+            x*2 + xx,
+            y*2 + yy,
+            z+1,
+            t.dow,
+            t.h,
+            (SELECT count(distinct clientid) FROM '||tablename||' WHERE ST_Intersects(the_geom_webmercator, CDB_XYZ_Extent(x*2 + xx, y*2 + yy, z+1)) and dow = t.dow and h = t.h)
+        FROM
+            t,
+            (VALUES (0, 0), (0, 1), (1, 1), (1, 0)) as c(xx, yy)
+        WHERE e >= '||threshold||' AND z < 25
+    ),
+    potential as(SELECT pid, id, x, y, z, dow, h, e FROM t WHERE e >= '||threshold||'),
+    cleaned as(
+        SELECT x, y, z, dow, h, e, coalesce(c, 0) as c
+        FROM
+            potential p1
+        left join
+            lateral(SELECT count(1) as c FROM potential where pid = p1.id) p2
+        ON 1=1
+    )
+    SELECT
+        dow, h,
+        ST_transform(CDB_XYZ_Extent(x, y, z), 3857) as the_geom,
+        e as occurrences
+    FROM cleaned
+    WHERE c = 0;';
+END;
+$$ language plpgsql IMMUTABLE;
